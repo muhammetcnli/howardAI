f6f11299d05f78eeeda3f750658c029d5b463f0a
muhammetcnli <howardroark756@gmail.com>
2025-03-09 18:30:37 +0300

    Enable webSecurity

--- howardAI/src/main/java/com/atlas/howardAI/HowardAiApplication.java
+++ howardAI/src/main/java/com/atlas/howardAI/HowardAiApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableWebSecurity
 public class HowardAiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HowardAiApplication.class, args);
 	}
