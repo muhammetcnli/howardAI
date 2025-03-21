ac4e408f2fe2987d0c3df0563d27c39f2d5d5e51
muhammetcnli <howardroark756@gmail.com>
2025-03-21 21:53:55 +0300

    Change ChatRepository type

--- howardAI/src/main/java/com/atlas/howardAI/repository/ChatRepository.java
+++ howardAI/src/main/java/com/atlas/howardAI/repository/ChatRepository.java
@@ -3,7 +3,9 @@ package com.atlas.howardAI.repository;
 import com.atlas.howardAI.entity.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatRepository extends JpaRepository<Chat,Long> {
+import java.util.UUID;
+
+public interface ChatRepository extends JpaRepository<Chat, UUID> {
 
 
 }
