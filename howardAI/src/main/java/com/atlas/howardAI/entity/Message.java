674aa00246a3e625acbea8164a8e6fa8c6dd238d
muhammetcnli <howardroark756@gmail.com>
2025-03-21 21:52:48 +0300

    Update message entity, add getter/setters

--- howardAI/src/main/java/com/atlas/howardAI/entity/Message.java
+++ howardAI/src/main/java/com/atlas/howardAI/entity/Message.java
@@ -1,16 +1,23 @@
 package com.atlas.howardAI.entity;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
 
+import java.sql.Types;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "message")
 public class Message {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    @JdbcTypeCode(java.sql.Types.VARCHAR)
+    private UUID id;
 
     @Column(columnDefinition = "LONGTEXT")
     private String content;
@@ -19,9 +26,58 @@ public class Message {
     private Boolean isUser;
 
     @Column(name = "created_at")
-    private LocalDate createdAt;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
     @ManyToOne
     @JoinColumn(name = "chat_id")
     private Chat chat;
+
+    public Message() {
+    }
+
+    public Message(String content, Boolean isUser, Chat chat) {
+        this.content = content;
+        this.isUser = isUser;
+        this.chat = chat;
+    }
+
+
+    // Getters
+    public UUID getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Boolean getIsUser() {
+        return isUser;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Chat getChat() {
+        return chat;
+    }
+
+    // Setters
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setIsUser(Boolean isUser) {
+        this.isUser = isUser;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setChat(Chat chat) {
+        this.chat = chat;
+    }
 }
